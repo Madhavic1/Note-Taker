54cce9c4627aeb4d956ca5ea011b028a6b95b5b5
Ganesh Chittella <madhavi.0108@gmail.com>
2020-02-29 17:48:54 -0800

    removed unwanted comments,console.log statements and alerts

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -8,7 +8,7 @@ var $noteList = $(".list-container .list-group");
 var activeNote = {};
 
 // A function for getting all notes from the db
-var getNotes = function() {
+var getNotes = function () {
   return $.ajax({
     url: "/api/notes",
     method: "GET"
@@ -16,7 +16,7 @@ var getNotes = function() {
 };
 
 // A function for saving a note to the db
-var saveNote = function(note) {
+var saveNote = function (note) {
   return $.ajax({
     url: "/api/notes",
     data: note,
@@ -25,9 +25,7 @@ var saveNote = function(note) {
 };
 
 // A function for deleting a note from the db
-var deleteNote = function(id) {
-  console.log(id);
-  
+var deleteNote = function (id) {
   return $.ajax({
     url: "api/notes/" + id,
     method: "DELETE"
@@ -35,10 +33,8 @@ var deleteNote = function(id) {
 };
 
 // If there is an activeNote, display it, otherwise render empty inputs
-var renderActiveNote = function() {
-  // alert('renderActiveNote');
+var renderActiveNote = function () {
   $saveNoteBtn.hide();
-// alert(activeNote.id);
   if (activeNote.id) {
     $noteTitle.attr("readonly", true);
     $noteText.attr("readonly", true);
@@ -53,20 +49,19 @@ var renderActiveNote = function() {
 };
 
 // Get the note data from the inputs, save it to the db and update the view
-var handleNoteSave = function() {
+var handleNoteSave = function () {
   var newNote = {
     title: $noteTitle.val(),
     text: $noteText.val()
   };
-// alert(newNote.title +':'+newNote.text)
-  saveNote(newNote).then(function(data) {
+  saveNote(newNote).then(function (data) {
     getAndRenderNotes();
     renderActiveNote();
   });
 };
 
 // Delete the clicked note
-var handleNoteDelete = function(event) {
+var handleNoteDelete = function (event) {
   // prevents the click listener for the list from being called when the button inside of it is clicked
   event.stopPropagation();
 
@@ -78,28 +73,27 @@ var handleNoteDelete = function(event) {
     activeNote = {};
   }
 
-  deleteNote(note.id).then(function() {
+  deleteNote(note.id).then(function () {
     getAndRenderNotes();
     renderActiveNote();
   });
 };
 
 // Sets the activeNote and displays it
-var handleNoteView = function() {
+var handleNoteView = function () {
   activeNote = $(this).data();
   renderActiveNote();
 };
 
 // Sets the activeNote to and empty object and allows the user to enter a new note
-var handleNewNoteView = function() {
+var handleNewNoteView = function () {
   activeNote = {};
-  // alert('22')
   renderActiveNote();
 };
 
 // If a note's title or text are empty, hide the save button
 // Or else show it
-var handleRenderSaveBtn = function() {
+var handleRenderSaveBtn = function () {
   if (!$noteTitle.val().trim() || !$noteText.val().trim()) {
     $saveNoteBtn.hide();
   } else {
@@ -108,7 +102,7 @@ var handleRenderSaveBtn = function() {
 };
 
 // Render's the list of note titles
-var renderNoteList = function(notes) {
+var renderNoteList = function (notes) {
   $noteList.empty();
 
   var noteListItems = [];
@@ -130,8 +124,8 @@ var renderNoteList = function(notes) {
 };
 
 // Gets notes from the db and renders them to the sidebar
-var getAndRenderNotes = function() {
-  return getNotes().then(function(data) {
+var getAndRenderNotes = function () {
+  return getNotes().then(function (data) {
     renderNoteList(data);
   });
 };
@@ -142,6 +136,7 @@ $newNoteBtn.on("click", handleNewNoteView);
 $noteList.on("click", ".delete-note", handleNoteDelete);
 $noteTitle.on("keyup", handleRenderSaveBtn);
 $noteText.on("keyup", handleRenderSaveBtn);
+
 
 // Gets and renders the initial list of notes
 getAndRenderNotes();
--- server.js
+++ server.js
@@ -14,14 +14,8 @@ app.use(express.static('public'));
 
 // Routes
 // =============================================================
-
-// Basic route that sends the user first to the AJAX Page
-app.get('/',(req,res)=>{
-    res.sendFile('index.html');
-});
-
-app.get('/notes',(req,res)=>{
-   res.sendFile(path.join(__dirname,'./public/notes.html'));
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
 app.get('/api/notes', (req, res) => {
@@ -33,31 +27,20 @@ app.get('/api/notes', (req, res) => {
     return res.json(jsonData);
 });
 
-app.post('/api/notes',(req,res)=>{
+app.post('/api/notes', (req, res) => {
     //new note
     var note = req.body;
-    //newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
-    note.id = note.title.replace(/\s+/g,"").toLowerCase();
-    console.log(note);
-
-    //reading db.json file
+    note.id = note.title.replace(/\s+/g, "").toLowerCase();
     var jsonData = readDbjsonFile();
     jsonData.push(note);
-    console.log(jsonData);
-    console.log('appending data ');
-    
-    fs.writeFileSync(path.join(__dirname, './db/db.json'),JSON.stringify(jsonData));
-
+    fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(jsonData));
     var data2 = fs.readFileSync(path.join(__dirname, './db/db.json'));
     var jsonData2 = JSON.parse(data2);
-    console.log(jsonData2);
-
     //return json data as response to the request
     return res.json(note);
 });
 
 app.delete('/api/notes/:id', (req, res) => {
-
     var deleteID = req.params.id;
     var jsonData = readDbjsonFile();
     for (let i = 0; i < jsonData.length; i++) {
@@ -66,14 +49,15 @@ app.delete('/api/notes/:id', (req, res) => {
             jsonData.splice(i, 1);
         }
     }
-    console.log('---');
-    console.log(jsonData);
-    fs.writeFileSync(path.join(__dirname, './db/db.json'),JSON.stringify(jsonData));
-
+    fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(jsonData));
     res.send('Deleting data from db.json')
 });
 
-app.listen(PORT,()=>{
+app.get('*', (req, res) => {
+    res.sendFile('index.html');
+});
+
+app.listen(PORT, () => {
     console.log(`Server is listening to the port ${PORT}`);
 });
 function readDbjsonFile() {
